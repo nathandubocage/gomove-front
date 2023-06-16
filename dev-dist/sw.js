d3c4d112b97fabd84e955f6ce7b4c7948067a9ef
Nathan Dubocage <nathandubocage@gmail.com>
2023-06-16 11:56:43 +0200

    Add `react-router-dom`

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.acbhue04cd"
+    "revision": "0.m58t6rrup4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
