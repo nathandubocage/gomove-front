1ce1e3d8596a6bccc4de07ebd5eeb0910b51b671
Nathan Dubocage <nathandubocage@gmail.com>
2023-06-16 12:15:57 +0200

    Add `login` and `register` components

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import App from "./App.tsx";
+import Login from "./components/Login/Login.tsx";
+import Register from "./components/Register/Register.tsx";
 
 import "./index.scss";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
