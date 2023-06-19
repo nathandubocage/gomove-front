c4bc449b6a8111c24b7718d0f5a9fd22d2a3094f
Nathan Dubocage <nathandubocage@gmail.com>
2023-06-19 23:39:37 +0200

    Add DayPicker

--- src/components/Answers/TravelDate/TravelDate.tsx
+++ src/components/Answers/TravelDate/TravelDate.tsx
@@ -1,4 +1,6 @@
-import { FC, Fragment } from "react";
+import { FC, Fragment, useState } from "react";
+import { DateRange, DayPicker } from "react-day-picker";
+import { addDays, format } from "date-fns";
 
 import { InputState } from "../../../types/input.spec";
 
@@ -7,26 +9,57 @@ import Input from "../../Input/Input";
 
 import "./TravelDate.scss";
 
-const TravelDate: FC<InputState> = ({ value, onChange }) => (
-  <Fragment>
-    <Heading content="Pendant combien de jours souhaitez vous voyager ?" />
+const pastMonth = new Date(2020, 10, 15);
 
-    <section className="travel">
-      <div className="travel__column">
-        <label className="travel__label" htmlFor="date">
-          Dates (optionnel)
-        </label>
+const TravelDate: FC<InputState> = ({ value, onChange }) => {
+  const defaultSelected: DateRange = {
+    from: pastMonth,
+    to: addDays(pastMonth, 4),
+  };
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
 
-        <Input
-          id="date"
-          type="date"
-          placeholder="Vos dates de voyage"
-          onChange={onChange}
-          value={value}
-        />
-      </div>
-    </section>
-  </Fragment>
-);
+  let footer = <p>Please pick the first day.</p>;
+  if (range?.from) {
+    if (!range.to) {
+      footer = <p>{format(range.from, "PPP")}</p>;
+    } else if (range.to) {
+      footer = (
+        <p>
+          {format(range.from, "PPP")}–{format(range.to, "PPP")}
+        </p>
+      );
+    }
+  }
+
+  return (
+    <Fragment>
+      <Heading content="Pendant combien de jours souhaitez vous voyager ?" />
+
+      <section className="travel">
+        <div className="travel__column">
+          <label className="travel__label" htmlFor="date">
+            Dates (optionnel)
+          </label>
+
+          <Input
+            id="date"
+            type="date"
+            placeholder="Vos dates de voyage"
+            onChange={onChange}
+            value={value}
+          />
+          <DayPicker
+            id="test"
+            mode="range"
+            defaultMonth={pastMonth}
+            selected={range}
+            footer={footer}
+            onSelect={setRange}
+          />
+        </div>
+      </section>
+    </Fragment>
+  );
+};
 
 export default TravelDate;
