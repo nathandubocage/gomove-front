3fac373cec4069c52e0fd7826a7c1d33ba468ad2
Nathan Dubocage <nathandubocage@gmail.com>
2023-06-19 11:13:37 +0200

    Finish to develop welcome screen

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -13,14 +13,15 @@ export default function Welcome() {
       <div className="welcome__heading">
         <img src={logoWelcome} alt="Gomove" className="welcome__logo" />
         <h1 className="welcome__heading-bold">
-          +1000 <br /> Destinations
+          <span>+1000</span>
+          <span>Destinations</span>
         </h1>
         <h2 className="welcome__heading-light">à travers le monde</h2>
       </div>
 
       <div className="welcome__actions">
         <Button content="Créer votre compte" />
-        <button>Se connecter</button>
+        <button className="welcome__login">Se connecter</button>
       </div>
     </section>
   );
