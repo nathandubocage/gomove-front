501022d6c149073ce9c2e0a5493cd0fbc3f3486b
Nathan Dubocage <nathandubocage@gmail.com>
2023-06-21 14:01:40 +0200

    Add modal

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lhm5i9mt2t"
+    "revision": "0.aiggt7hv9io"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Account/Criteria/Criteria.tsx
+++ src/components/Account/Criteria/Criteria.tsx
@@ -1,15 +1,63 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Heading from "../../Heading/Heading";
 import CardSetting from "../../Card/Setting/CardSetting";
 
 import arrowLeft from "../../../assets/icons/arrow_left.svg";
+import cross from "../../../assets/icons/cross.svg";
 
 import "./Criteria.scss";
 import { useNavigate } from "react-router-dom";
 
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
 export default function Criteria() {
   const navigate = useNavigate();
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const clearCriteria = () => {
+    closeModal();
+  };
+
+  const customStyles = {
+    content: {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      height: "200px",
+      borderRadius: "20px",
+      width: "80%",
+    },
+    overlay: {
+      backgroundColor: "rgba(0,0,0,0.5)",
+      boxShadow:
+        "inset -12px -12px 36px rgba(23, 140, 199, 0.12), inset 12px 12px 36px rgba(255, 255, 255, 0.5)",
+    },
+    closeButton: {
+      position: "absolute",
+      top: "25px",
+      right: "20px",
+      background: "none",
+      border: "none",
+      fontSize: "large",
+      cursor: "pointer",
+    },
+  };
 
   return (
     <Fragment>
@@ -27,9 +75,24 @@ export default function Criteria() {
           </button>
 
           <div className="header__delete">
-            <button>Effacer tous les critères</button>
+            <button onClick={openModal}>Effacer tous les critères</button>
           </div>
         </header>
+
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+        >
+          <button style={customStyles.closeButton} onClick={closeModal}>
+            <img src={cross} alt="Fermer la fenêtre" />
+          </button>
+          <h2 className="modal__heading">
+            Êtes-vous sûr d’effacer tous les critères ?
+          </h2>
+          <button onClick={clearCriteria}>Oui</button>
+          <button onClick={closeModal}>Non</button>
+        </Modal>
 
         <Heading content="Mes critères" />
 
