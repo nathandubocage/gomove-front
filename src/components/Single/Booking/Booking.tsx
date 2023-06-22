e49f424bd105ab7e9c55d4476772aa37a9819c89
Nathan Dubocage <nathandubocage@gmail.com>
2023-06-22 11:19:44 +0200

    Add filter on booking tab

--- src/components/Single/Booking/Booking.tsx
+++ src/components/Single/Booking/Booking.tsx
@@ -1,7 +1,54 @@
 import { Fragment } from "react";
+import { useUserStore } from "../../../store/useUserStore";
 
 import "./Booking.scss";
 
 export default function Booking() {
-  return <Fragment>Hello</Fragment>;
+  const { userCriterias } = useUserStore();
+
+  return (
+    <Fragment>
+      <div className="booking__filters flex justify-center gap-1.5 w-full">
+        <a
+          href="/passengers"
+          className="booking__filter bg-[rgba(255,255,255,.8)] px-2 py-1 rounded font-space-grotesk grow"
+        >
+          <p className="text-[#594D62] text-[10px]">Voyageurs</p>
+          <p className="text-[#30213B] text-[14px] font-bold">
+            {userCriterias?.passengers! || 2}
+          </p>
+        </a>
+
+        <a
+          href="/price"
+          className="booking__filter bg-[rgba(255,255,255,.8)] px-2 py-1 rounded font-space-grotesk grow"
+        >
+          <p className="text-[#594D62] text-[10px]">Budget</p>
+          <p className="text-[#30213B] text-[14px] font-bold">
+            &lt; {userCriterias?.budget! || "250"} €
+          </p>
+        </a>
+
+        <a
+          href="/destination"
+          className="booking__filter bg-[rgba(255,255,255,.8)] px-2 py-1 rounded font-space-grotesk grow"
+        >
+          <p className="text-[#594D62] text-[10px]">Destination</p>
+          <p className="text-[#30213B] text-[14px] font-bold">
+            {userCriterias?.destination! || "-"}
+          </p>
+        </a>
+
+        <a
+          href="/travel-date"
+          className="booking__filter bg-[rgba(255,255,255,.8)] px-2 py-1 rounded font-space-grotesk grow"
+        >
+          <p className="text-[#594D62] text-[10px]">Date</p>
+          <p className="text-[#30213B] text-[14px] font-bold">
+            {userCriterias?.departureDate!}
+          </p>
+        </a>
+      </div>
+    </Fragment>
+  );
 }
