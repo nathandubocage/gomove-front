36e1f3396f512fb00a973731c7aaaa963aced3ff
Jérémy Piorun <jpiorun@insitaction.com>
2023-06-22 14:50:55 +0200

    feat add dynamic travel details

--- src/components/Single/Single.tsx
+++ src/components/Single/Single.tsx
@@ -1,27 +1,32 @@
 import { Fragment, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import Booking from "./Booking/Booking";
 import City from "./City/City";
 
 import arrowLeft from "../../assets/icons/arrow_left.svg";
 
-import backgroundSingle from "../../assets/fake/berlin_single.png";
-
 import "./Single.scss";
+import { TTravel } from "../../types/travel.spec";
+import getCountryFlagEmoji from "../../utils/get-country-flag";
 
 export default function Single() {
-  const { id: number } = useParams();
-
-  const navigate = useNavigate();
+  const location = useLocation();
+  const travel: TTravel = location.state.travel;
 
   const [activeTab, setActiveTab] = useState("tabOne");
 
   return (
     <Fragment>
       <header
-        style={{ backgroundImage: `url(${backgroundSingle})` }}
-        className="header header--sized"
+        style={{
+          backgroundImage: `linear-gradient(
+          180deg,
+          transparent 0%,
+          rgba(255, 255, 255, 1) 100%
+        ),url(${travel.destination.image})`,
+        }}
+        className="header header--sized flex flex-col-reverse single-header"
       >
         <button
           className="header__back header__back--single"
@@ -33,6 +38,11 @@ export default function Single() {
             className="header__arrow"
           />
         </button>
+
+        <h2 className="my-4 font-integral-cf font-bold text-4xl">
+          {travel.destination.city}{" "}
+          {getCountryFlagEmoji(travel.destination.country)}
+        </h2>
       </header>
 
       <section className="single">
--- src/components/TravelCard/TravelCard.tsx
+++ src/components/TravelCard/TravelCard.tsx
@@ -4,18 +4,25 @@ import roundTrip from "../../assets/icons/round_trip.svg";
 import heartEmpty from "../../assets/icons/heart_empty.svg";
 import heartFull from "../../assets/icons/heart_full.svg";
 import "./TravelCard.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useUserStore } from "../../store/useUserStore";
 
-export default function TravelCard(props: TTravel) {
+export default function TravelCard({
+  travel,
+  onTravelChange,
+}: {
+  travel: TTravel;
+  onTravelChange: (travel: TTravel) => void;
+}) {
   const { userFavourites, addUserFavourite, removeUserFavourite } =
     useUserStore();
   const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const cardRef = useRef(null);
 
   const checkIfFavourite = () => {
     if (userFavourites.length > 0) {
       userFavourites.forEach((favourite) => {
-        if (favourite.id === props.id) {
+        if (favourite.id === travel.id) {
           setIsFavorite(true);
         }
       });
@@ -24,27 +31,52 @@ export default function TravelCard(props: TTravel) {
 
   useEffect(() => {
     checkIfFavourite();
+
+    const options = {
+      root: null, // Utilise la fenêtre comme racine d'observation
+      rootMargin: "0px",
+      threshold: 0.5, // Définit le seuil d'intersection à 50%
+    };
+
+
+    const observer = new IntersectionObserver((entries) => {
+      const isVisible = entries[0].isIntersecting;
+      if (isVisible) {
+        onTravelChange(travel);
+      }
+    }, options);
+
+    if (cardRef.current) {
+      observer.observe(cardRef.current);
+    }
+
+    return () => {
+      if (cardRef.current) {
+        observer.unobserve(cardRef.current);
+      }
+    };
   }, []);
 
-  return props.transport.length > 0 && props.transport[0].price !== null ? (
+  return travel.transport.length > 0 && travel.transport[0].price !== null ? (
     <div
+      ref={cardRef}
       className="h-full flex flex-col-reverse snap-center bg-cover"
-      style={{ backgroundImage: `url(${props.destination.image})` }}
+      style={{ backgroundImage: `url(${travel.destination.image})` }}
     >
       <div className="h-2/5 py-16 flex flex-col travel-card-infos px-6 text-[#30233E] gap-2">
         <h2 className="flex font-bold font-integral-cf uppercase tracking-wider gap-2">
           <span className="text-4xl h-full leading-normal">
-            {props.destination.city}
+            {travel.destination.city}
           </span>
           <span className="text-5xl">
-            {getCountryFlagEmoji(props.destination.country)}
+            {getCountryFlagEmoji(travel.destination.country)}
           </span>
         </h2>
 
         <div className="flex flex-col gap-2">
           <p className="font-space-grotesk font-bold text-normal">
             à partir de
-            <span className="text-3xl"> {props.transport[0].price} €</span>
+            <span className="text-3xl"> {travel.transport[0].price} €</span>
           </p>
           <p className="flex gap-2">
             <img src={roundTrip} alt="Modifier" />
@@ -55,7 +87,7 @@ export default function TravelCard(props: TTravel) {
               <button
                 onClick={() => {
                   setIsFavorite(!isFavorite);
-                  addUserFavourite(props);
+                  addUserFavourite(travel);
                 }}
               >
                 <img src={heartEmpty} alt="Ajouter aux favoris" />
@@ -64,7 +96,7 @@ export default function TravelCard(props: TTravel) {
               <button
                 onClick={() => {
                   setIsFavorite(!isFavorite);
-                  removeUserFavourite(props.id);
+                  removeUserFavourite(travel.id);
                 }}
               >
                 <img src={heartFull} alt="Ajouter aux favoris" />
